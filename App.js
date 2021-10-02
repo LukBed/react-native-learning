b08857bd4134b156e49f9d809e12a2eab037ef76
Lukasz Bednorz <lukasz.bednorz@snipersoft.net>
2021-10-02 08:47:38 +0200

    style sheet

--- App.js
+++ App.js
@@ -1,23 +1,34 @@
 import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import { SafeAreaView, Text, StyleSheet } from 'react-native';
+
+//better performance also!
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignContent: 'center',
+  },
+  paragraph: {
+    backgroundColor: 'grey',
+    color: 'white',
+    margin: 24,
+    fontSize: 18,
+    textAlign: 'center',
+  },
+  nestedText: {
+    fontWeight: 'bold',
+  },
+});
 
 export default function App() {
   return (
     //RN is based on flexbox, axes are reversed in comparison to web
-    <SafeAreaView
-      style={{ flex: 1, justifyContent: 'center', alignContent: 'center' }}>
-      <Text
-        style={{
-          backgroundColor: 'grey',
-          color: 'white',
-          margin: 24,
-          fontSize: 18,
-          textAlign: 'center',
-        }}>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.paragraph}>
         Mauris pretium in neque in sagittis. Donec accumsan turpis ac lacus
         ullamcorper, in interdum eros hendrerit. Duis eu congue libero. Maecenas
         convallis interdum erat quis euismod.
-        <Text style={{ fontWeight: 'bold' }}>
+        <Text style={styles.nestedText}>
           Maecenas finibus feugiat venenatis.
         </Text>
       </Text>
