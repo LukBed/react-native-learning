4f754213dbafc85da75f5d00d53d10a8a3699e9f
Lukasz Bednorz <lukasz.bednorz@snipersoft.net>
2021-10-02 08:41:16 +0200

    styling inline

--- App.js
+++ App.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import { Text, Platform, SafeAreaView } from 'react-native';
+import { SafeAreaView, Text } from 'react-native';
 
 export default function App() {
   return (
-    //SafeAreaView - only IOS11+, safe margin, very useful, works as normal view on Android
-    <SafeAreaView>
+    //RN is based on flexbox, axes are reversed in comparison to web
+    <SafeAreaView
+      style={{ flex: 1, justifyContent: 'center', alignContent: 'center' }}>
       <Text
         style={{
-          backgroundColor: Platform.OS === 'android' ? 'green' : 'red',
+          backgroundColor: 'grey',
+          color: 'white',
+          margin: 24,
+          fontSize: 18,
+          textAlign: 'center',
         }}>
-        {Platform.OS}
+        Mauris pretium in neque in sagittis. Donec accumsan turpis ac lacus
+        ullamcorper, in interdum eros hendrerit. Duis eu congue libero. Maecenas
+        convallis interdum erat quis euismod.
+        <Text style={{ fontWeight: 'bold' }}>
+          Maecenas finibus feugiat venenatis.
+        </Text>
       </Text>
-      <Text
-        style={{
-          ...Platform.select({
-            android: {
-              backgroundColor: 'orange',
-            },
-          }),
-        }}>
-        {Platform.OS}
+      <Text>
+        Nunc a aliquam lacus. Aliquam leo quam, feugiat nec fermentum a, iaculis
+        eget nisl. Vivamus ut tellus quis velit dictum bibendum. Nulla nec
+        consequat turpis. Etiam congue auctor tellus, vel dictum nulla faucibus
+        vitae.
       </Text>
     </SafeAreaView>
   );
